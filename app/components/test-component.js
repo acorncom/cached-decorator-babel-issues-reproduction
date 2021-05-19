8ecc062ff4b625d4881a1a4a410aa0d5cb524e5e
David Baker <dbaker@acornwebconsultants.com>
2021-05-19 13:32:37 +0300

    Add use of the @cached decorator, which blows things up

--- app/components/test-component.js
+++ app/components/test-component.js
@@ -1,10 +1,11 @@
 import Component from '@glimmer/component';
-import { tracked } from '@glimmer/tracking';
+import { tracked, cached } from '@glimmer/tracking';
 
 export default class TestComponentComponent extends Component {
   @tracked firstName = 'David';
   @tracked lastName = 'Baker';
 
+  @cached
   get fullName() {
     return `${this.firstName} ${this.lastName}`;
   }
